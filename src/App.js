5934838828d7069b5833dfbfcb80612066039ef5
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-06-13 22:36:00 +0530

    Hii

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Typography } from '@mui/material';
 import * as React from 'react';
-import './App.css'
-import MiniDrawer from './components/MiniDrawer';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <MiniDrawer />
+    // <div className="App">
+    //   <MiniDrawer />
+    // </div>
+    <div style={{height: "100vh", width: "100vw", display: "grid", placeItems: "center"}}>
+        <Typography variant="h1" sx={{color: "navy", textAlign: "center", display: "grid", placeItems: "center"}}>
+            I Love you 
+            <FavoriteIcon sx={{color: "red", fontSize: "10rem"}} />
+            Moon
+        </Typography>
+        
     </div>
   );
 }
