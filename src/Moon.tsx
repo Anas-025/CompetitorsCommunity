003c26bcb598f37c92371e0cd05093e3165356b6
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-06-13 22:27:48 +0530

    sea touches the reflection

---
+++ src/Moon.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Moon() {
+  return (
+    <div style={{height: "100vh", width: "100vw", display: "grid", placeItems: "center"}}>
+        <Typography variant="h1" sx={{color: "navy"}}>
+            Moon
+        </Typography>
+        
+    </div>
+  )
+}
+
+export default Moon
