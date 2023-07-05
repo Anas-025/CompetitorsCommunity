db656193ac4303777eaebc870d1c572e11626a1b
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-07-05 23:29:17 +0530

    Added new features

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import MiniDrawer from '../components/MiniDrawer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer />
+      skdfkjh
+    </div>
+  );
+}
+
+export default App;
